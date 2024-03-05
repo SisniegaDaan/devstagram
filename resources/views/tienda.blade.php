70d8d47c4140a5db7f15fd519f0bc7576e1e6caf
Daan Solis <daansol@hotmail.com>
2024-03-05 16:03:28 -0600

    Creacion de Layout

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('titulo')
+Tienda Virtual
+@endsection
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <style></style>
-    </head>
-    <body class="antialiased">
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="/tienda">Tienda</a>
-        </nav>
-        <h1>Tienda Virtual</h1>
-    </body>
-</html>
+@section('contenido')
+<p>Cuerpo del contenido de la tienda virtual</p>
+@endsection
