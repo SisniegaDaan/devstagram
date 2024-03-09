f43c95295fe34212d78160b1f5e5c1bb77cd1a48
Daan Solis <daansol@hotmail.com>
2024-03-09 10:44:35 -0600

    Auth Views Forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,5 +5,38 @@ Crea tu cuenta
 @endsection
 
 @section('contenido')
-<p>Formulario</p>
+    <div class="md:flex">
+        <div class="md:w-1/2">
+            <p>Imagen aquí</p>
+        </div>
+        <div class="md:w-1/2">
+            <form>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input id="name" name="name" type="text" placeholder="Tu nombre" class="border p-3 w-full rounded-lg"/>
+                </div>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input id="username" name="username" type="text" placeholder="Tu username" class="border p-3 w-full rounded-lg"/>
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input id="email" name="email" type="text" placeholder="Tu email" class="border p-3 w-full rounded-lg"/>
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Contraseña
+                    </label>
+                    <input id="password" name="password" type="text" placeholder="Crea tu contraseña" class="border p-3 w-full rounded-lg"/>
+                </div>
+            </form>
+
+        </div>
+    </div>
 @endsection
