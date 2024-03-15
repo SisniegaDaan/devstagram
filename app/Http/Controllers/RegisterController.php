0563b2fc50f2617e527586771a449d72841c9cf2
Daan Solis <daansol@hotmail.com>
2024-03-14 21:00:57 -0600

    Before push

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -14,13 +15,19 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
+        // Convirtiendo el valor que ingresa el usuario a un valor
+        // apto para ser usado en url
+        $request->request->add(['username' => Str::slug($request->username)]);
+    
+        // Validación de campos
         $this->validate($request, [
             'name'=> ['required', 'max:30'],
             'username'=> ['required', 'unique:users', 'min:3', 'max:20'],
             'email'=> ['required', 'email', 'unique:users', 'min:3', 'max:20'],
-            'password'=> ['required', 'confirmed'],
+            'password'=> ['required', 'confirmed']
         ]);
         
+        // Creación del usuario post a la conversión y validación de datos
         User::create([
             'name' => $request->name,
             'email' => $request->email,
