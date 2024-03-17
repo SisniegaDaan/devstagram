1c5d94a7d7b9250fbcca5acec3e39b50d5524181
Daan Solis <daansol@hotmail.com>
2024-03-16 20:08:01 -0600

    Protección de ruta y comienzo de login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view("auth/login");
+    }
+
+    public function post(Request $request)
+    {
+        auth()->attempt($request->only("email","password"));
+        return redirect()->route('posts.index');
+    }
 }
--- resources/views/feed.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('titulo')
-Feed Devstagram
-@endsection
-
-@section('contenido')
-<p>Feed de Devstagram</p>
-@endsection
