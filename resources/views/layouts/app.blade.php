769e4027f98643aca43c03fa2d08b656d4de1aa3
Daan Solis <daansol@hotmail.com>
2024-03-22 10:15:56 -0600

    Alexa push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
                     @auth
                         <a class="font-bold uppercase text-gray-600 text-sm" href="#">Hola: <span class="font-normal">{{auth()->user()->username}}</span></a>
-                        <a class="font-bold uppercase text-gray-600 text-sm" href="#">Cerrar Sesión</a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">Cerrar Sesión</a>
                     @endauth
                     
                     @guest
